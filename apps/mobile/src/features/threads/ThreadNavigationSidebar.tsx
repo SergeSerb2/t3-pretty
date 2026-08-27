f8a2995c9262b5a1641bee6799910d99fca338f8
Serge Alex Serbinenko <129629680+SergeSerb2@users.noreply.github.com>
2026-08-27 14:58:22 -0700

    fix(mobile): let Android brand row grow

--- apps/mobile/src/features/threads/ThreadNavigationSidebar.tsx
+++ apps/mobile/src/features/threads/ThreadNavigationSidebar.tsx
@@ -1323,7 +1323,7 @@ function ThreadNavigationSidebarPane(
             onPress={props.onOpenEnvironmentSettings}
             size="pageTitle"
             brand={
-              <View className="h-12 flex-1 justify-center">
+              <View className="min-h-12 flex-1 justify-center">
                 <CompactBrandTitle allowFontScaling={Platform.OS === "android"} />
               </View>
             }
